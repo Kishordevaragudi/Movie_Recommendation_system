3b3d72fff7979c8146a07a9685844958b45d67ed
kishordevaragudi <kishordev36@gamil.com>
2023-09-15 12:51:02 +0530

    update

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "Movie-Recommender-System-Using-Machine-Learning"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Movie_Recommendation_system"
+AUTHOR_USER_NAME = "Kishordevaragudi"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit']
 
@@ -18,9 +18,9 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="entbappy73@gmail.com",
+    author_email="Kishordev36@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     install_requires=LIST_OF_REQUIREMENTS
 )
